ba31000e3daaf634204f18206c91f8e33ea9949a
Digitalyz <76618474+digitalyz@users.noreply.github.com>
2022-05-12 23:26:08 +0200

    Rename settings (1).js to settings.js

--- src/web-ui/public/settings (1).js
+++
@@ -1,1 +0,0 @@
-window.rekognitionSettings = {"apiGateway":"https://fj4bxxxkj2.execute-api.eu-west-1.amazonaws.com/PROD","cognitoIdentityPool":"eu-west-1:82ff02d6-609e-4b41-8227-62150816affb","region":"eu-west-1"};
